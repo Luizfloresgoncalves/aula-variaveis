dac59eeab161351cbd871ec102eaf2d99b32d3ab
João S <joao.jpg678@gmail.com>
2023-08-16 21:58:19 -0300

    criado aula-variaveis

---
+++ index.js
@@ -0,0 +1,22 @@
+/* exercicio 1 : Eu acho que no primeiro console vai aparecer o (10), 
+E no segundo console vai aparecer (10,5)*/
+
+// exercicio 2 : Eu acho que no console do exercicio vai aparecer (10,10,10) //
+
+/* exercicio 3 : ele cria uma variavel para quantas horas trabalha e outra variavel para o valor do salário,
+ o alert calcula quanto recebe por cada hora trabalhada ,
+ eu colocaria let horas = prompt( para horas de trabalho)
+e let salário = prompt ( para salario)*/
+
+//exercicios de escrita de código//
+
+//let nome 
+//let idade 
+
+//console.log(typeof nome)
+//console.log(typeof idade)// resposta d : undefined ,apareceu pois ainda não definiu valores e strings para as variaveis//
+
+const nome = prompt("Qual é seu nome?")
+const idade = prompt("Qual é sua idade?")
+
+console.log(typeof nome ,typeof idade)
