32e646a1b59b22167c75f72a4471e1983bcc5803
João S <joao.jpg678@gmail.com>
2023-08-17 21:13:26 -0300

    criado aula variaveis

--- index.js
+++ index.js
@@ -8,15 +8,42 @@ E no segundo console vai aparecer (10,5)*/
  eu colocaria let horas = prompt( para horas de trabalho)
 e let salário = prompt ( para salario)*/
 
-//exercicios de escrita de código//
+//exercicios de escrita de código , exercicio 1//
 
-//let nome 
-//let idade 
+//let nome //resposta: a
+//let idade //resposta: b
 
-//console.log(typeof nome)
-//console.log(typeof idade)// resposta d : undefined ,apareceu pois ainda não definiu valores e strings para as variaveis//
+//console.log(typeof nome)//resposta: c
+//console.log(typeof idade)//resposta: c
+// resposta d : undefined ,apareceu pois ainda não definiu valores e strings para as variaveis//
 
-const nome = prompt("Qual é seu nome?")
+/*const nome = prompt("Qual é seu nome?")
 const idade = prompt("Qual é sua idade?")
+console.log(typeof nome, typeof idade)*/ //resposta: e
 
-console.log(typeof nome ,typeof idade)
+// resposta f : apareceu string ,string ,tanto para nome quanto para idade
+
+/*let nome = prompt ("Qual é o seu nome")
+let idade = prompt ("Qual é sua idade ")
+console.log(" Olá " + nome +". Você tem "+ idade + " anos ")
+alert(" Olá " + nome +" .Você tem "+ idade + " anos ")*/ //resposta: g
+
+//exercicio 2 da escrita de código
+
+/*const pergunta1 = prompt ("Você esta usando uma roupa azul hoje?")
+const pergunta2 = prompt ("Você gosta de jogar futebol?")
+const pergunta3 = prompt ("Você trabalha?")*/ //resposta :a
+
+//console.log(" Você esta usando uma roupa azul hoje? " + pergunta1 + " .Você gosta de jogar futebol? " + pergunta2 + " .Você trabalha? " +pergunta3)//resposta :b
+
+//exercicio 3 da escrita de codigo
+
+let a = 10
+let b = 25
+let c = a
+//a = b
+//b = c
+console.log(a,b)
+console.log("O valor de c é "+ a)
+console.log("O novo valor de a é "+ b )
+console.log("O novo valor de b é "+ c )
